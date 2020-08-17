e6dcff179463af3f4b8c4c9d554bbc8be8bc4cc8
smilerac <smilerac15@gmail.com>
2020-08-17 10:35:15 +0545

    Updates

---
+++ precache-manifest.339a8bae0262271819cdb44b7ebc7fb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515c691b7be177b1f8b173d4fca7f3dc",
+    "url": "/Leapfrog-HackerNews-Assignment/index.html"
+  },
+  {
+    "revision": "e8de463893c01e355cca",
+    "url": "/Leapfrog-HackerNews-Assignment/static/css/main.6e51fdcd.chunk.css"
+  },
+  {
+    "revision": "ee3782b497893b04d96b",
+    "url": "/Leapfrog-HackerNews-Assignment/static/js/2.9fc9a19a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Leapfrog-HackerNews-Assignment/static/js/2.9fc9a19a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8de463893c01e355cca",
+    "url": "/Leapfrog-HackerNews-Assignment/static/js/main.d01fec76.chunk.js"
+  },
+  {
+    "revision": "ad86aa9bfc5f24e0dbb1",
+    "url": "/Leapfrog-HackerNews-Assignment/static/js/runtime-main.ee850afb.js"
+  }
+]);
